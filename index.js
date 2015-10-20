f89980a44f2d68a2df619099445db816193e5e82
twozh <xiaogen-zhang@163.com>
2015-10-20 13:50:37 +0800

    export model User

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ exports.route = require('./routes/user-route.js');
 /* core api like CRUD */
 exports.apiRoute = require('./routes/user-route-api.js');
 
-
+/* export User modle */
+exports.User = require('./routes/user-model.js');
